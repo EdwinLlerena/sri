f826078f3b30ac166669c6908d9a79f01cdfa81d
Edwin LLerena <marwin99@hotmail.com>
2022-11-04 15:41:11 -0500

    cambiando idioma datatable

--- src/public/datatable.js
+++ src/public/datatable.js
@@ -1,4 +1,25 @@
 
-$(document).ready(function () {
-    $('#docu').DataTable();
-});
+//$(document).ready(function () {
+//   $('#docu').DataTable();
+//});
+
+
+$(document).ready(function() {
+
+    $('#docu').DataTable( 
+         {
+  "language": {
+    "info": "Pagina _PAGE_ de _PAGES_",
+    "lengthMenu":     "Mostrar _MENU_ registros",
+    "search":         "Buscar:",
+    "paginate": {
+        "first":      "Primera",
+        "last":       "Última",
+        "next":       "Siguiente",
+        "previous":   "Anterior"
+    },
+  }
+}
+     );
+
+} );
